e483c33dc321756e93b421682deecdfaefa20703
wasim <wasimysdev@gmail.com>
2024-04-27 14:37:46 +0200

    Add CORS handling in Elysia app
    The code has been updated to import and use "@elysiajs/cors" in the Elysia application. Along with this, routes using 'app.all' now have an additional middleware for CORS handling. Headers related to content-encoding and transfer-encoding are now removed from the response.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+app.use(cors());
+app.all("v1/*", async (data) => {
+   
+   return fetch(`https://api.anthropic.com${data.path}`, {
+	  method: data.request.method,
+	  headers: {
+		 "x-api-key": data.headers["x-api-key"],
+		 "anthropic-version": data.headers["anthropic-version"]
+		 
+	  } as HeadersInit,
+	  body: JSON.stringify(data.body)
+   });
+   
+}, {
+   mapResponse({response}) {
+	  if (response instanceof Response) {
+		 (response as Response).headers.delete("content-encoding");
+		 (response as Response).headers.delete("transfer-encoding");
+	  }
+   }
+   
+});
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
